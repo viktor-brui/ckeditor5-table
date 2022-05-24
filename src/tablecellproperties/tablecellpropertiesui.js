b8d6ba6801c1bbb36aeabf4a6b2946111ce114e8
Viktor Brui <v.bruy@akdev.by>
2022-05-24 11:02:18 +0300

    ckeditor5-table change settings

--- src/tablecellproperties/tablecellpropertiesui.js
+++ src/tablecellproperties/tablecellpropertiesui.js
@@ -103,7 +103,7 @@ export default class TableCellPropertiesUI extends Plugin {
 			const view = new ButtonView( locale );
 
 			view.set( {
-				label: t( 'Cell properties' ),
+				label: t( 'Cell properties123' ),
 				icon: tableCellProperties,
 				tooltip: true
 			} );
